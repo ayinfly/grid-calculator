62f4889f8bcb297a4e912e4d5a4782c8396bf5b1
ayinfly <ayinflyschool@gmail.com>
2023-03-25 14:16:54 -0700

    Add decimal functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const box = document.getElementById('box');
 let val = "0";
+let clr = false;
+let dot = false;
 
 function update() {
     box.textContent = val;
@@ -8,10 +10,17 @@ function update() {
 function key(num) {
     if (num == 'null') {
         val = "0";
+        dot = false;
     } else if (num == 'dec') {
         val = (+val / 100).toString();
+        dot = true;
     } else if (num == 'neg') {
         val = (0 - +val).toString();
+    } else if (num == '.') {
+        if (!dot) {
+            val += '.'
+            dot = !dot;
+        }
     } else if (val == "0") {
         val = num.toString();
     } else {
